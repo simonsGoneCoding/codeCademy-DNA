8be179e68ef2caea782ef29076dfd0f151bfdb9b
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-11-24 16:16:51 +0000

    Find the start and stop codon.

--- Dna.java
+++ Dna.java
@@ -5,5 +5,15 @@ public class Dna { // program that determines whether there is a protein in a st
         String dna2 = "ATGCGATACGTGA";
         String dna3 = "ATTAATATGTACTGA";
 
+        // generic DNA var that can be set to any DNA sequence
+        String dna = dna1;
+        int length = dna.length();
+        System.out.println("Length: " + length);
+
+        // finding start (ATG) and stop (TGA) codon
+        int startCodon = dna.indexOf("ATG");
+        int stopCodon = dna.indexOf("TGA");
+        System.out.println("Start: " + startCodon);
+        System.out.println("Stop: " + stopCodon);
     }
 }
