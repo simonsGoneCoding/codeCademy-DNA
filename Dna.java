7e150cb6665c0da6cca1886901fbea81ed5f69c2
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-11-24 16:39:51 +0000

    Create find protein logic.

--- Dna.java
+++ Dna.java
@@ -1,12 +1,16 @@
 public class Dna { // program that determines whether there is a protein in a strand of DNA
 
+    public void findProtein(String dna) {
+
+    }
+
     public static void main(String[] args) {
         String dna1 = "ATGCGATACGCTTGA";
         String dna2 = "ATGCGATACGTGA";
         String dna3 = "ATTAATATGTACTGA";
 
         // generic DNA var that can be set to any DNA sequence
-        String dna = dna1;
+        String dna = dna2;
         int length = dna.length();
         System.out.println("Length: " + length);
 
@@ -15,5 +19,14 @@ public class Dna { // program that determines whether there is a protein in a st
         int stopCodon = dna.indexOf("TGA");
         System.out.println("Start: " + startCodon);
         System.out.println("Stop: " + stopCodon);
+
+        // Find the protein:
+        if (startCodon != -1 && stopCodon != -1 && (stopCodon - startCodon) % 3 == 0) {
+            System.out.println("Condition 1, 2 and 3 are fufilled.");
+            String protein = dna.substring(startCodon, stopCodon + 3);
+            System.out.println("Protein: " + protein);
+        } else {
+            System.out.println("No protein found.");
+        }
     }
 }
